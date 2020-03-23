855799141e23117d15f25a81c1e1fd2b5ed4255c
Julia McNeill <juliammcneill@gmail.com>
2020-03-23 01:03:04 -0400

    Add files via upload

--- js/javascript3.js
+++ js/javascript3.js
@@ -1,33 +1,14 @@
-let allAddButtons = document.querySelectorAll('.addbutton');
-for (let i = 0; i < allAddButtons.length; i++) {
-
-  allAddButtons[i].onclick = function(event) {
-
-    event.preventDefault();
-
-    let newElement = document.createElement('div');
-    newElement.innerHTML = event.currentTarget.previousElementSibling.value;
-    newElement.style.backgroundColor = event.currentTarget.dataset.color;
-
-    let delButton = document.createElement('button');
-    delButton.innerHTML = 'delete';
-    newElement.appendChild(delButton);
-
-    delButton.onclick = function(event) {
-      event.currentTarget.parentElement.innerHTML = "";
-    }
-
-    event.currentTarget.previousElementSibling.value = "";
-
-    document.getElementById('results').appendChild(newElement);
-  }
-
-}
-
 let allTabs = document.querySelectorAll('.tab');
 for (let i = 0; i < allTabs.length; i++) {
 
   allTabs[i].onclick = function(event) {
+
+    for (let j = 0; j < allTabs.length; j++) {
+      allTabs[j].classList.remove('selected');
+    }
+
+    event.currentTarget.classList.add('selected');
+
     let allPanels = document.querySelectorAll('.content');
     for (let j = 0; j < allPanels.length; j++) {
       allPanels[j].classList.add('hidden');
@@ -37,4 +18,108 @@ for (let i = 0; i < allTabs.length; i++) {
     document.getElementById(contentID).classList.remove('hidden');
   }
 
+}
+
+let allAddButtons = document.querySelectorAll('.addbutton');
+for (let i = 0; i < allAddButtons.length; i++) {
+
+  allAddButtons[i].onclick = function(event) {
+
+    event.preventDefault();
+
+    let newText = document.createElement('span');
+    newText.innerHTML = event.currentTarget.previousElementSibling.value;
+    let newElement = document.createElement('div');
+    newElement.appendChild(newText);
+    newElement.style.backgroundColor = event.currentTarget.dataset.color;
+    newElement.classList.add('listItem');
+    newElement.classList.add(event.currentTarget.dataset.type);
+
+    let delButton = document.createElement('button');
+    delButton.innerHTML = 'delete';
+    delButton.classList.add('button');
+    newElement.appendChild(delButton);
+
+    delButton.onclick = function(event) {
+      event.currentTarget.parentElement.innerHTML = "";
+      newElement.classList.remove('listItem');
+    }
+
+    let moveDownButton = document.createElement('button');
+    moveDownButton.innerHTML = '▽';
+    moveDownButton.classList.add('button');
+    moveDownButton.classList.add('moveDownButton');
+    newElement.appendChild(moveDownButton);
+
+    moveDownButton.onclick = function(event) {
+      if (event.currentTarget.parentNode.nextElementSibling) {
+        event.currentTarget.parentNode.parentNode.insertBefore(event.currentTarget.parentNode.nextElementSibling, event.currentTarget.parentNode);
+      }
+    }
+
+    let moveUpButton = document.createElement('button');
+    moveUpButton.innerHTML = '△';
+    moveUpButton.classList.add('button');
+    moveUpButton.classList.add('moveUpButton');
+    newElement.appendChild(moveUpButton);
+
+    moveUpButton.onclick = function(event) {
+      if(event.currentTarget.parentNode.previousElementSibling) {
+        event.currentTarget.parentNode.parentNode.insertBefore(event.currentTarget.parentNode, event.currentTarget.parentNode.previousElementSibling);
+      }
+    }
+
+    document.getElementById('results').appendChild(newElement);
+
+    event.currentTarget.previousElementSibling.value = "";
+
+  }
+
+}
+
+function filterFunction() {
+  var listItems = document.getElementsByClassName("listItem");
+  var option = document.getElementById("filter").value;
+
+  for (let i = 0; i < listItems.length; i++) {
+      if (option == "all") {
+        listItems[i].style.display = "";
+        continue;
+      }
+      listItems[i].style.display = "none";
+      if (listItems[i].classList.contains(option)) {
+        listItems[i].style.display = "";
+      }
+  }
+}
+
+document.onclick = updateMoveButtonsAndStrikethrough;
+function updateMoveButtonsAndStrikethrough() {
+  var listItems = document.getElementsByClassName("listItem");
+  for (let i = 0; i < listItems.length; i++) {
+
+    var itemText = listItems[i].querySelector("span");
+
+    itemText.onclick = function(event) {
+      if (event.currentTarget.style.textDecoration == "line-through") {
+        event.currentTarget.style.textDecoration = "none";
+      } else {
+        event.currentTarget.style.textDecoration = "line-through";
+      }
+    }
+
+    var itemMoveUpButton = listItems[i].querySelector(".moveUpButton");
+    var itemMoveDownButton = listItems[i].querySelector(".moveDownButton");
+
+    itemMoveUpButton.style.visibility = "hidden";
+    itemMoveDownButton.style.visibility = "hidden";
+
+    if (listItems[i-1]) {
+      itemMoveUpButton.style.visibility = "visible";
+    }
+
+    if (listItems[i+1]) {
+      itemMoveDownButton.style.visibility = "visible";
+    }
+  }
 }
